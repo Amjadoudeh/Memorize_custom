1ab0af2266bad56dda3837a315268ef349c5919f
Amjadoudeh <Amjad.oudeh88@gmail.com>
2021-10-11 18:19:01 +0200

    Initial Commit

---
+++ Memorize_custom/Memorize_customApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize_customApp.swift
+//  Memorize_custom
+//
+//  Created by Amjad Oudeh on 11.10.21.
+//
+
+import SwiftUI
+
+@main
+struct Memorize_customApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
