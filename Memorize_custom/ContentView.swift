659be1b44108dd7045204977b5cb02b8b2736e3b
Amjadoudeh <Amjad.oudeh88@gmail.com>
2021-10-11 18:50:55 +0200

    created card component and it faces up and down!

--- Memorize_custom/ContentView.swift
+++ Memorize_custom/ContentView.swift
@@ -9,10 +9,53 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack{
+        CardView(content:"Amjad")
+        }
+        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+        .padding()
     }
 }
+
+
+
+
+
+
+
+// card!
+
+struct CardView: View {
+    var content : String
+   @State var isFaceUp : Bool = true
+    
+    var body : some View {
+        let shape = RoundedRectangle(cornerRadius: 25.0)
+        ZStack {
+        if isFaceUp {
+            shape.fill().foregroundColor(.white)
+            shape.strokeBorder(lineWidth: 3.0)
+            Text(content).font(.largeTitle)
+        } else {
+            shape.fill()
+            }
+        }
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
